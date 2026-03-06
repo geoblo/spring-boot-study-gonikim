a0f858bdb2e57f989cb2dda5ff32fafdf9609189
geoblo <geoblo@naver.com>
2026-03-06 16:43:50 +0900

    first commit

---
+++ lombok/src/main/java/com/example/demo/Member.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Member {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+}
