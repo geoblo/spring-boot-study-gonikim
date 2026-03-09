ac168f0ac8517ac00af00490123533f5ae67aec2
geoblo <geoblo@naver.com>
2026-03-09 09:54:10 +0900

    up

---
+++ ioc/src/main/java/com/example/demo/EspressoMachine.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public class EspressoMachine implements CoffeeMachine {
+    @Override
+    public String brew() {
+        return "Brewing coffee with Espresso Machine";
+    }
+}
