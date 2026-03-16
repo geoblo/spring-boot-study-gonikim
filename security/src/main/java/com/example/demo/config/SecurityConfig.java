0a2566e4459ed119e3237f14bc44e483deb97be0
geoblo <geoblo@naver.com>
2026-03-16 14:30:21 +0900

    up

--- security/src/main/java/com/example/demo/config/SecurityConfig.java
+++ security/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
@@ -11,12 +13,27 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
 // 스프링 시큐리티 관련 설정을 위한 빈을 정의할 수 있는 클래스
 @Configuration
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/", "/home").permitAll() // 모든 사용자에게 허용
+                        .requestMatchers("/member/**").hasAuthority("ROLE_ADMIN") // 로그인한 사용자 중에 해당 권한이 있는 사용자만 접근 허용
+                        .anyRequest().authenticated() // 그 외의 모든 요청은 인증된 사용자만 접근 허용
+                )
+                .formLogin(Customizer.withDefaults())
+                .logout(Customizer.withDefaults());
+
+        return http.build();
+    }
+
 
     // 임시1: 메모리 기반 사용자 인증
 //    @Bean
--- security/src/main/resources/data.sql
+++ security/src/main/resources/data.sql
@@ -1,12 +1,12 @@
-INSERT INTO users (username, password, enabled)
-VALUES ('user', '$2a$12$hWlC4LQUyqYc2Cw.G4XehOioYNMF44lkNGH2UQ/BjRrGinWmNCNxK', TRUE);
-
-INSERT INTO users (username, password, enabled)
-VALUES ('admin', '$2a$12$hWlC4LQUyqYc2Cw.G4XehOioYNMF44lkNGH2UQ/BjRrGinWmNCNxK', TRUE);
-
-INSERT INTO authorities (username, authority) VALUES ('user', 'ROLE_USER');
-INSERT INTO authorities (username, authority) VALUES ('admin', 'ROLE_USER');
-INSERT INTO authorities (username, authority) VALUES ('admin', 'ROLE_ADMIN');
+--INSERT INTO users (username, password, enabled)
+--VALUES ('user', '$2a$12$hWlC4LQUyqYc2Cw.G4XehOioYNMF44lkNGH2UQ/BjRrGinWmNCNxK', TRUE);
+--
+--INSERT INTO users (username, password, enabled)
+--VALUES ('admin', '$2a$12$hWlC4LQUyqYc2Cw.G4XehOioYNMF44lkNGH2UQ/BjRrGinWmNCNxK', TRUE);
+--
+--INSERT INTO authorities (username, authority) VALUES ('user', 'ROLE_USER');
+--INSERT INTO authorities (username, authority) VALUES ('admin', 'ROLE_USER');
+--INSERT INTO authorities (username, authority) VALUES ('admin', 'ROLE_ADMIN');
 
 -- full custom
 INSERT INTO member(name, email, password)
